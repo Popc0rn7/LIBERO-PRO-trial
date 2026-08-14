8d9a19754e3235607125c4eeb98cb8e5a54ff814
popc0rn <wqy20031003@sjtu.edu.cn>
2026-08-14 12:53:59 +0800

    Remove legacy policy_eval

--- policy_eval/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Client/server evaluation bridge for LIBERO-PRO."""
-
-from .protocol import ACTION_DIM, PROTOCOL_VERSION, PolicyClient, ProtocolError
-
-__all__ = ["ACTION_DIM", "PROTOCOL_VERSION", "PolicyClient", "ProtocolError"]
